3c2aadc5d171d5d6bff55dd82873eedfde86d40b
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-04-29 21:25:52 +0200

    create action created for offers controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -9,6 +9,9 @@ class OffersController < ApplicationController
     @pet = Pet.find_by(id: params[:pet_id])
     authorize @offer
   end
+  def create
+    
+  end
 #   private
 #   def get_params
 #     params.request {"species"=>"asd", "location"=>"yX", "start_date"=>"0111-11-11", "end_date"=>"0001-11-11"}
