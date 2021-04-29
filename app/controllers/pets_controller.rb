6fa1e4149d8bd76eb5d4c2f97521d0cc308bf125
fabian <fabiludos@gmail.com>
2021-04-29 21:07:38 +0200

    statin pet crud

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,11 +1,21 @@
 class PetsController < ApplicationController
   def new
     @pet = Pet.new
+    authorize @pet
   end
 
   def create
+    @pet = Pet.new(pet_params)
+    authorize @pet
+    @pet.user = current_user
+    @pet.save
+    redirect_to pet_path(@pet)
   end
 
   def show
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :description, :picture, :location, :species)
   end
 end
