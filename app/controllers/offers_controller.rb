684d596319d84d3720bf70d524cd69abe56da3e0
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-04-29 20:08:12 +0200

    seach for offer via only pet type in policy implemented

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,12 +1,12 @@
 class OffersController < ApplicationController
 
   def index
-    @offers = policy_scope(params)
+    @offers = policy_scope(Offer)
   end
 
   
-  private
+#   private
 #   def get_params
-#     {"species"=>"asd", "location"=>"yX", "start_date"=>"0111-11-11", "end_date"=>"0001-11-11"}
-#   end
+#     params.request {"species"=>"asd", "location"=>"yX", "start_date"=>"0111-11-11", "end_date"=>"0001-11-11"}
+#    end
 end
--- app/policies/pet_policy.rb
+++ app/policies/pet_policy.rb
@@ -1,7 +1,7 @@
 class PetPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(species: params(:species))
     end
   end
 
