914e8e93d970975f2865a206e29dc51c59a7e4ea
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-05-01 11:31:35 +0200

    validations added to pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,5 +2,5 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :offers, class_name: "offer", foreign_key: "reference_id"
   has_one_attached :picture
-  validates :picture, presence: true
+  validates :picture, :name, :location, :species, :description, presence: true
 end
