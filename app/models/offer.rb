e6a3e096ae1a59e05d1f460152a1721c9d460053
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-05-01 12:13:06 +0200

    offer gets created when valdidations pass and user is autherized to create offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,13 @@
 class Offer < ApplicationRecord
   has_many :requests
   belongs_to :pet
-  validates :start_date, :end_date, pesence: true
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date
+
+  private
+  def end_date_after_start_date 
+    errors.add(:end_date, "should be after start date") if end_date <= start_date
+  end
+
 end
+
