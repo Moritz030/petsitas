8220bc642da453fb1b607939a62c7d2043c40c66
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-05-04 20:15:56 +0200

    faker gem added and seed file includes now seeding of users with pets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+pets_array = ["Dog", "Cat", "Crocodile", "Rat", "Tiger", "Perrot", "Panda"]
+puts "Creating Users with pets..."
+3.times do 
+  user = User.new(email: Faker::Internet.email)
+  user.password = "123456"
+  user.save!
+  rand(1..4).times do
+    species = pets_array.sample
+    pet = Pet.new(user: user, name: Faker::TvShows::StarTrek.character, species: species, description: Faker::TvShows::BojackHorseman.quote, location: Faker::Address.city)
+    pet.picture.attach(io: File.open("db/seed_images/#{species.downcase}.jpg"), filename: 'dog.jpg', content_type: 'image/jpg')
+    pet.save!
+  end
+end
+
+puts "Users created with pets"
+
+
+
+# 10.times do
+
+# end
