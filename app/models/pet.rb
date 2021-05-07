e7e78a6fc063bb93e2df6d2c3cf31fd7f7d0de64
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-05-07 21:42:23 +0200

    geocoding added to Pet model and seed file changed, so geocoding works with seeds

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,4 +4,7 @@ class Pet < ApplicationRecord
   has_many :requests, through: :offers, dependent: :destroy
   has_one_attached :picture
   validates :user, :picture, :name, :location, :species, :description, presence: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ puts "All Users destroyed!"
 # end
 # puts "All Offers destroyed!"
 
-
+locations_array = ["Apolda", "Berlin", "Hamburg", "Trier", "Potsdam", "Erfurt", "Frankfurt", "Leipzig", "München", "Dresden", "Stuttgart", "Weimar", "Jena", "Chemnitz", "Flensburg", "Bamberg", "Bochum", "Dortmund", "Mainz", "Hannover", "Waren", "Saarbrücken"]
 pets_array = ["Dog", "Cat", "Crocodile", "Rat", "Tiger", "Perrot", "Panda"]
 puts "Creating Users with pets and offers..."
 3.times do 
@@ -38,9 +38,10 @@ puts "Creating Users with pets and offers..."
   user.save!
   rand(1..4).times do
     species = pets_array.sample
-    pet = Pet.new(user: user, name: Faker::TvShows::StarTrek.character, species: species, description: Faker::TvShows::BojackHorseman.quote, location: Faker::Address.city)
+    pet = Pet.new(user: user, name: Faker::TvShows::StarTrek.character, species: species, description: Faker::TvShows::BojackHorseman.quote, location: locations_array.sample)
     pet.picture.attach(io: File.open("db/seed_images/#{species.downcase}.jpg"), filename: 'dog.jpg', content_type: 'image/jpg')
     pet.save!
+    sleep(1)
     rand(1..3).times do
       time_period = time_rand
       offer = Offer.new(pet: pet, start_date: time_period[0], end_date: time_period[1])
